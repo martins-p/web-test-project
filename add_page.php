327b31897ae85e1b89ed541eee0358f0b7a93f41
martins-p <pludo89@gmail.com>
2019-05-10 13:33:52 +0300

    Update

--- add_page.php
+++ add_page.php
@@ -10,20 +10,36 @@
 </head>
 <body>
     <div><h2>Product Add</h2></div>
-    <form action="" method="post">
+    <form action="" method="post" id="new-product-form">
         SKU <input type="text" name="SKU"><br>
         Name <input type="text" name="name"><br>
         Price <input type="text" name="price"><br>
-        Type <select name="type" id="selected-product-type">
+        Type <select name="type" id="select-product-type">
                 <?php while($row = $attributeObject->fetch_assoc()): ?>
                     <option> <?=$row['type']?></option>
                 <?php endWhile; ?>
             </select>
+
         <div>
-            <h3></h3>
+            <div id="size-container">
+                Size <input type="text" name="value"><br>
+                <p>Info about size.</p>
+            </div>
+            <div id="size-container">
+                Height <input type="number" id="furniture-height"> cm<br>
+                Width <input type="number" id="furniture-width"> cm<br>
+                Length <input type="number" id="furniture-length"> cm<br>
+                <input type="hidden" name="value">
+                <p>Info about Weight.</p>
+            </div>
+            <div id="weight-container">
+                Size <input type="text" name="value"><br>
+                <p>Info about size.</p>
+            </div>
+            
         </div>
     </form>
-
+    <button type="submit" form="new-product-form" value="Submit">Save</button>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 <script src="main.js"></script>
 </body>
--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 $("#selected-product-type").change(function() {
     $("#hide-test").hide();
 });
+
+$("#save-button").click(function join_size_values () {
+    var height = $("#furniture height").val();
+    var width = $("#furniture width").val();
+    var length = $("#furniture length").val();
+    $("#furniture-size").val(height + "x" + width + "x" + length);
+});
+
+$("#select-product-type").select(function(){
+    var selection = $("#select-product-type option:selected").text();
+    $("#" + selection + "container").show();
+});
